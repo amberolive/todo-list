fc4cbd054d8dcaebc48065e4e20bc7115845b361
Amber <amberolive@gmail.com>
2017-02-02 12:32:13 -0500

    Add items base functionality (#1)
    * Generate application route.
    
    * Generate application controller.
    
    * Generate todo model.
    
    * Change status to complete.
    
    * Add item functionality.

---
+++ app/application/controller.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+      addItem() {
+        var newItem = this.store.createRecord('todo', {
+          complete: false,
+          item: this.get('item'),
+
+        });
+        newItem.save();
+
+      }
+    }
+});
