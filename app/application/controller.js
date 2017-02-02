326b3dff2345d4e93c05900f059acfb91a8bfd56
Amber <amberolive@gmail.com>
2017-02-02 15:08:53 -0500

    chore: Remove commented code (#3)
    * Generate application route.
    
    * Generate application controller.
    
    * Generate todo model.
    
    * Change status to complete.
    
    * Add item functionality.
    
    * Checking in.
    
    * Remove .travis.yml
    
    * Remove extra code.

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+<<<<<<< HEAD
+
+=======
+>>>>>>> 9bb0f26ee684fb3ca74e78ba956b176825557172
   actions: {
       addItem() {
         var newItem = this.store.createRecord('todo', {
