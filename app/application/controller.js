d7930de362a2c638bde5ec5c9de378beff9d4138
AmberRivera <amberolive@gmail.com>
2017-02-02 12:57:04 -0500

    Checking in.

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+
   actions: {
       addItem() {
         var newItem = this.store.createRecord('todo', {
--- app/application/route.js
+++ app/application/route.js
@@ -4,6 +4,17 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('todo');
+    //this.store.unloadAll();
+    //this.store.findAll('todo').invoke('destroyRecord');
+
+    /*this.store.findRecord('todo', '-Kbadw190yUTcsg73qss').then(function(todo) {
+      todo.destroyRecord(); // => DELETE to /posts/1
+    });*/
+
+    /*this.store.get('todo').foreach(function(item) {
+      item.destroyRecord();
+    });
+    return this.store.findAll('todo');*/
   }
 
 });
