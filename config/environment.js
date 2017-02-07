ea0144fac95705988e8a1ad9c97657eeb7c92931
AmberRivera <amberolive@gmail.com>
2017-02-06 21:17:56 -0500

    Move firebase config.

--- config/environment.js
+++ config/environment.js
@@ -39,16 +39,7 @@ module.exports = function(environment) {
     }
   };
 
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDH9QNBxWBrHC9gojP7GXyKQJyxuMm6psg",
-    authDomain: "todolistfirebase-6f7d8.firebaseapp.com",
-    databaseURL: "https://todolistfirebase-6f7d8.firebaseio.com",
-    storageBucket: "todolistfirebase-6f7d8.appspot.com",
-    messagingSenderId: "338122648095"
-  };
-
-  firebase.initializeApp(config);
+  
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
